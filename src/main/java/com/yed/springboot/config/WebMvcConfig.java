5e3cc2cedc8a8565d9f7b9de9cee32a31cc678ca
yed <yanerdong8@sina.com>
2018-11-23 17:03:30 +0800

    我的主队 网站初建

---
+++ src/main/java/com/yed/springboot/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.yed.springboot.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/upload/**").addResourceLocations("classpath:/upload/");
+    }
+
+}
